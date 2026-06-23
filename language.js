1e9a69b0718e209c1c31afb84e516acf1eaaa4b4
Wolf-3317 <gankokcheng7@gmail.com>
2026-06-23 12:51:05 +0800

    Add files via upload

---
+++ language.js
@@ -0,0 +1,47 @@
+export const languages = [
+    {
+        name: "HTML",
+        backgroundColor: "#E2680F",
+        color: "#F9F4DA",
+    },
+    {
+        name: "CSS",
+        backgroundColor: "#328AF1",
+        color: "#F9F4DA",
+    },
+    {
+        name: "JavaScript",
+        backgroundColor: "#F4EB13",
+        color: "#1E1E1E",
+    },
+    {
+        name: "React",
+        backgroundColor: "#2ED3E9",
+        color: "#1E1E1E",
+    },
+    {
+        name: "TypeScript",
+        backgroundColor: "#298EC6",
+        color: "#FFFFFF",
+    },
+    {
+        name: "Node.js",
+        backgroundColor: "#539E43",
+        color: "#FFFFFF",
+    },
+    {
+        name: "Python",
+        backgroundColor: "#FFD43B",
+        color: "#1E1E1E",
+    },
+    {
+        name: "Ruby",
+        backgroundColor: "#CC342D",
+        color: "#FFFFFF",
+    },
+    {
+        name: "Assembly",
+        backgroundColor: "#6E4C13",
+        color: "#FFFFFF",
+    }
+]
